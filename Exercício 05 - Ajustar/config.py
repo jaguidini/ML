687fea6fdbfd1ca2d9738d334c638489b899e897
José Guidini <jaguidini@outlook.com>
2019-10-19 15:32:52 -0300

    Removido pasta antiga

--- Exercício 05 - Ajustar/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-ROOT_PATH = os.path.abspath(os.curdir)
-if ROOT_PATH.endswith('\\') == False:
-    ROOT_PATH += '\\'
-
-
--- Exercício 05/readDiabetes.py
+++
@@ -1,8 +0,0 @@
-import joblib
-
-classifier = joblib.load('models/diabetes.joblib')
-param=[[0,100,40,35,168,43.1,2.288,33]]
-print("Predict")
-print(classifier.predict(param))
-print("Predict proba")
-print(classifier.predict_proba(param))
